f1da44866e4226faa3b28c4ed41aec61a91d4d29
SalvatierraFran <franco14_93@hotmail.com>
2016-03-30 01:03:08 -0300

    Creada la clase Calculadora para las operaciones matemáticas.

---
+++ Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio15
+{
+    class Calculadora
+    {
+        public static void Calcular(double numUno, double numDos, string operacion)
+        {
+            double resultado;
+
+            if (operacion == "+")
+            {
+                resultado = numUno + numDos;
+                mostrar(resultado);
+            }
+            else if (operacion == "-")
+            {
+                resultado = numUno - numDos;
+                mostrar(resultado);
+            }
+            else if (operacion == "*")
+            {
+                resultado = numUno * numDos;
+                mostrar(resultado);
+            }
+            else
+            {
+                if(Validar(numDos)==true)
+                {
+                    resultado = numUno / numDos;
+                    mostrar(resultado);
+                }
+            }
+        }
+
+        private static bool Validar(double SegundoNumero)
+        {
+            bool rdo = false;
+            
+            if (SegundoNumero != 0)
+            {
+                rdo = true;
+            }
+
+            return rdo;
+        }
+
+        public static void mostrar(double Final)
+        {
+            Console.Write("\nEl resultado es: " + Final);
+        }
+    }
+}
